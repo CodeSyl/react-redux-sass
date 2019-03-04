43d1280e1c2cf2d60e921700501dad192cf1eaaf
Sylvain LAPART <sylbtz@Sylvains-MacBook-Pro.local>
2019-03-04 16:30:17 +0100

    Splited redux config

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import reducers from './redux/reducers';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import store from './redux/store';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-const store = createStore(
-  reducers,
-  composeWithDevTools(applyMiddleware(thunk))
-);
 
 ReactDOM.render(
   <Provider store={store}>
