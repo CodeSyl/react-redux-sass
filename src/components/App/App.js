6ed777f86846c4579e525e3c84faadd9b25a40af
Sylvain LAPART <sylbtz@Sylvains-MacBook-Pro.local>
2019-03-04 22:04:22 +0100

    Added and remove some style and files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
+  <div className="app">
+    <div className="container">
       <h1 className="title">Boilerplate React Redux Thunk</h1>
-      <p className="subtitle">Boilerplate React Redux Thunk</p>
-    </header>
+      <p className="subtitle">With Json server to launch a fake rest api</p>
+    </div>
   </div>
 );
 
